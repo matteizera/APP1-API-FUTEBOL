ca5cb57b3b57afa59f06841222338cafc6603f04
Julio Muller <juliodolago@gmail.com>
2021-05-29 15:28:16 -0300

    Mensagem ao iniciar servidor alterada

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const routes = require('./routes')
+
 const app = express()
 const port = 3333
 
 app.use(express.json())
-app.use(express.urlencoded({extended:false}))
+app.use(express.urlencoded({ extended: false }))
 app.use(routes)
 
 app.listen(port, () => {
-  console.log(`Servidor aberto na porta ${port}`)
+  console.log(`Servidor executando em http://localhost:${port}`)
 })
