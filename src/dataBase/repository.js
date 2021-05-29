c61b32b63e086e45ec6e34e4466a28df88336982
Julio Muller <juliodolago@gmail.com>
2021-05-29 15:21:23 -0300

    Operações com BD refatorado para padrão repositório

---
+++ src/dataBase/repository.js
@@ -0,0 +1,47 @@
+const database = require('./data.json')
+
+const sequence = {
+  nextId(data) {
+    return data.reduce((maxId, team) => Math.max(maxId, team.id), 1) + 1
+  },
+}
+
+class Repository {
+  constructor(collection) {
+    this._collection = collection
+  }
+
+  get() {
+    return this._collection
+  }
+
+  find(id) {
+    return this._collection.find((item) => item.id == id) // eslint-disable-line eqeqeq
+  }
+
+  save(data) {
+    if (data.id) {
+      const item = this.find(data.id)
+
+      return item && Object.assign(item, data)
+    }
+
+    this._collection.push({
+      id: sequence.nextId(this._collection),
+      ...data,
+    })
+
+    return this._collection.slice(-1)
+  }
+
+  delete(id) {
+    const itemIndex = this._collection.findIndex((item) => item.id == id) // eslint-disable-line eqeqeq
+    const item = this._collection[itemIndex] || null
+
+    return item && this._collection.splice(itemIndex, 1)[0]
+  }
+}
+
+module.exports = {
+  teamsRepository: new Repository(database.teams),
+}
--- src/routes.js
+++ src/routes.js
@@ -1,35 +1,35 @@
 const express = require('express')
-const routes = express.Router()
-const DataBase = require('./dataBase')
 const validateTeam = require('./util/validateTeam')
+const { teamsRepository } = require('./dataBase/repository')
 
-//Create team
-routes.post("/teams", (req, res) => {
+const routes = express.Router()
 
+// Create team
+routes.post('/teams', (req, res) => {
   const { name, city, state, serie, payment, titles } = req.body
   const team = { name, city, state, serie, payment, titles }
-  const {isTeamValid, msg} = validateTeam(team)
+  const { isTeamValid, msg } = validateTeam(team)
+
   if (isTeamValid) {
-    const fixedTitles = team.titles.map(title => Math.floor(title))
+    const fixedTitles = team.titles.map((title) => Math.floor(title))
+
     team.titles = fixedTitles
-
-    res.status(200).json(DataBase.saveTeam(team))
-
+    res.status(200).json(teamsRepository.save(team))
   } else {
-
     res.status(400).json({ msg })
-
   }
 })
 
-//List teams
-routes.get('/teams', (req, res) => {
-})
-//Find team
-routes.get('/teams/:id', (req, res) => {})
-//Edit team
-routes.put("/teams/:id", (req, res) => {})
-//Remove team
-routes.delete("/teams/:id", (req, res) => {})
+// List teams
+routes.get('/teams', (req, res) => {})
 
-module.exports =  routes
+// Find team
+routes.get('/teams/:id', (req, res) => {})
+
+// Edit team
+routes.put('/teams/:id', (req, res) => {})
+
+// Remove team
+routes.delete('/teams/:id', (req, res) => {})
+
+module.exports = routes
